03f5b694eb17e14d89feeee02ff60a3de84523b0
migueldf10 <migueldf10@gmail.com>
2020-06-04 17:56:15 +0200

    Now displaying individual patients

--- index.js
+++ index.js
@@ -2,26 +2,9 @@
 const express = require('express')
 // create server
 const app = express()
-const patients = require("./patients");
+const patients = require("./patientsData");
 
 
-// handler function
-function onRequest(request, response) {
-	{ // handler callback
-		// send data to client
-		response.send(`
-		<html>
-			<head>
-				<title>This is the ${request.path} page</title>
-			</head>
-		
-			<body>
-				<h1>This is the ${request.path} page</h1>
-				<p>Here is the default response</p>
-			</body>
-		</html>`)
-	}
-}
 
 
 function patientsView(request, response) {
@@ -47,10 +30,12 @@ function patientsView(request, response) {
 		${patients.map((patient, index) => {
 		return (
 			`<tr>
-				<td>` + index + '</td>'
+			
+				<td><a href="/patients/${patient.id}">` + index + '</a></td>'
 			+ Object.values(patient).map((value) => {
 				return ('<td>' + value + '</td>')
 			}).join('')
+
 			+ '</tr>'
 		)
 	}).join('')}
@@ -60,21 +45,59 @@ function patientsView(request, response) {
 }
 
 
-function patientView(req, res) {
+function patientView(request, response) { // handler callback
+	const patient = patients.find(patient => patient.id == request.params.id)
+	console.log(patients)
 
+	if (patient) {
+		response.send(`
+			<html>
+			<head>
+			<title>This is the PATIENT page</title>
+			</head>
+			
+			<body>
+			<h1>This is the Patient page of the individual with ID ${patient.id} and name ${patient.firstName}</h1>
+			<table>
+			<tr>
+				<th>
+					Name
+				</th>
+			</tr>
+			<tr>
+				<td>
+					${patient.firstName}
+				</td>
+			</tr>
+			</table>
+
+			<a href="/">Back to patients</a>
+			
+			</body >
+			</html > `)
+	} else {
+		response.send(`<html>
+			<head>
+			<title>This is the PATIENT page</title>
+			</head>
+			
+			<body>
+				<h1>NOTHING WAS FOUND</h1>
+				<a href="/">Back to patients</a>
+		
+			
+			</body >
+			</html > `)
+	}
 }
 
 
-// register GET /hello endpoint
-app.get(
-	'/', // route to listen on
-	onRequest // callback runs when the route is requested
-)
+
 
 
 // register GET endpoints
 app.get(
-	'/patients', // route
+	'/', // route
 	patientsView
 
 )
@@ -82,9 +105,7 @@ app.get(
 // register GET /hello endpoint
 app.get(
 	'/patients/:id', // declares a parameter named title
-	(request, response) => { // handler callback
-		response.send(request.params.id)
-	}
+	patientView
 )
 
 // 3000 is common
